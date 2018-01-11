1120a544c223d75df844a7b51dd0d022751c0b8d
章旭楠 <zhang_xn1314@163.com>
2018-01-11 16:36:23 +0800

    初次提交

---
+++ src/main/java/com/vdong/file/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.vdong.file.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    String hello() {
+        return "hello";
+    }
+}
