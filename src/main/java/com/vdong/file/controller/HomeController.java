442a92fb577c6544137a0ec3c122675b8613ae72
章旭楠 <zhang_xn1314@163.com>
2018-01-16 09:21:14 +0800

    修改docker配置文件

--- src/main/java/com/vdong/file/controller/HomeController.java
+++ src/main/java/com/vdong/file/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/")
     String hello() {
-        return "hello zhangxunan";
+        return "hello zhangxunan ";
     }
 }
