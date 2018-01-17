5bbadcb56df2bab7b0d349ed5b7f1eaf72c5fce4
章旭楠 <zhang_xn1314@163.com>
2018-01-17 16:43:13 +0800

    引入模板引擎

--- src/main/java/com/vdong/file/controller/HomeController.java
+++ src/main/java/com/vdong/file/controller/HomeController.java
@@ -1,12 +1,23 @@
 package com.vdong.file.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Map;
+
+@Controller
 public class HomeController {
     @RequestMapping("/")
     String hello() {
         return "hello zhangxunan v1.0.0";
     }
+
+    @RequestMapping("/hello")
+    public String hello(Map<String, Object> map) {
+
+        map.put("hello", "from TemplateController.helloHtml 章");
+        return "/hello";
+    }
+
 }
