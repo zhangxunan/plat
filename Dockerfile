333be94acd57de5060c5cd583d01ad1e218231f1
章旭楠 <zhang_xn1314@163.com>
2018-01-18 14:32:14 +0800

    修改pom

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM hub.c.163.com/wuxukun/maven-aliyun:3-jdk-8
 ADD pom.xml /tmp/build/
 ADD src /tmp/build/src
 #构建应用
-RUN cd /tmp/build && mvn clean package \
+RUN cd /tmp/build && mvn clean -P prob package \
 #拷贝编译结果到指定目录
 && mv target/*.jar /app.jar \
 #清理编译痕迹
--- src/main/java/com/vdong/file/App.java
+++ src/main/java/com/vdong/file/App.java
@@ -1,14 +1,21 @@
 package com.vdong.file;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class, args);
+public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(App.class, args);
+        String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
+        for (String profile : activeProfiles) {
+            logger.warn("Spring Boot 使用profile为:{}", profile);
+        }
     }
 }
