5f60a3d165a08c2a31fec10093b318256f193ef0
章旭楠 <zhang_xn1314@163.com>
2018-01-24 09:14:30 +0800

    添加redis

--- src/main/java/com/vdong/file/controller/HomeController.java
+++ src/main/java/com/vdong/file/controller/HomeController.java
@@ -2,15 +2,17 @@ package com.vdong.file.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @Controller
 public class HomeController {
 
+
     @RequestMapping("/hello")
     public String hello(Map<String, Object> map) {
+        //Config config = new Config();
+        //RedissonClient redisson = Redisson.create(config);
 
         map.put("hello", "章旭楠。222。fuck2222");
         return "hello";
